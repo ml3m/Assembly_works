36cdbcbb38f297e693ca107c04209f1e121107b7
ml3m <vladutifju6@gmail.com>
2024-04-10 14:47:56 +0300

    fibonaci works

--- c_examples/fibonaci_nth.c
+++ c_examples/fibonaci_nth.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-int fibonacciSum(int n) {
-    int prev = 0, current = 1, next, sum = 0;
+int main() {
+    int n;
+
+    printf("Enter the number of Fibonacci numbers: ");
+    scanf("%d", &n);
+
+    int prev = 0, current = 1, next;
     
     if (n == 1) 
         return prev;
     
-    for (int i = 2; i <= n; i++) {
-        sum += current;
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", current);
         next = prev + current;
+   
+
         prev = current;
         current = next;
+
     }
-    
-    return sum; // Adding 1 because the sum includes the first Fibonacci number which is 0.
-}
-
-int main() {
-    int n;
-    printf("Enter the number of Fibonacci numbers: ");
-    scanf("%d", &n);
-
-    int sum = fibonacciSum(n);
-    printf("Sum of the first %d Fibonacci numbers is: %d\n", n, sum);
-
     return 0;
 }
